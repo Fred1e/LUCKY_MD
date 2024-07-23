4f2b82480f215d5ac2559755574b13a01039b49e
FredieTech <frediezra60@gmail.com>
2024-07-23 20:29:47 +0300

    Update menu1.js

--- commandes/menu1.js
+++ commandes/menu1.js
@@ -1,184 +1,80 @@
 const util = require('util');
-
 const fs = require('fs-extra');
-
 const { zokou } = require(__dirname + "/../framework/zokou");
-
 const { format } = require(__dirname + "/../framework/mesfonctions");
-
 const os = require("os");
-
 const moment = require("moment-timezone");
-
 const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
 
-
-
-zokou({ nomCom: "menu", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
-
+zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
     let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
-
     let { cm } = require(__dirname + "/../framework//zokou");
-
     var coms = {};
-
     var mode = "public";
-
     
-
     if ((s.MODE).toLocaleLowerCase() != "yes") {
-
         mode = "private";
-
     }
 
 
-
-
-
     
-
-
 
     cm.map(async (com, index) => {
-
         if (!coms[com.categorie])
-
             coms[com.categorie] = [];
-
         coms[com.categorie].push(com.nomCom);
-
     });
 
-
-
-    moment.tz.setDefault(s.TZ);
-
-
+    moment.tz.setDefault('Etc/GMT');
 
 // Créer une date et une heure en GMT
-
 const temps = moment().format('HH:mm:ss');
-
 const date = moment().format('DD/MM/YYYY');
 
-
-
   let infoMsg =  `
-
-┏━━𒈒Lucky MD 2024 ━━┓
-┃   Mode: ${mode}
-┃   User : ${s.OWNER_NAME}
-┃
-┣━𒈒lucky md new vision 𒈒━➠
-┗━━━𒈒by Fredie tech 𒈒━━┛\n\n`;
+*LUCKY MD V5 AVAILABLE MENUS* 
 
 
+    ▸ *commander* : ${cm.length} 
+    ▸ *rom* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+    ▸ *uptime* : ${os.platform()}
+    ▸ *theme* : *fredie tech*
+
+> LUCKY MD V5 WA BOT
+> POWERED BY FREDIE TECH 💎\n${readmore}`;
     
-
 let menuMsg = `
-┏━━━━━━━━━━━━━━┓
-┣❏lucky md v5 
-┣❏©Fredie 🫶
-┗━━━━━━━━━━━━━━┛\n
-
-
-
-
-𒈒lucky md V5 cmds𒈒
-`;
-
-
-
-    for (const cat in coms) {
-
-        menuMsg += `┏━━━━━⚼ ${cat}`;
-
-        for (const cmd of coms[cat]) {
-
-            menuMsg += `
-┃➠ ${cmd}`;
-
-        }
-
-        menuMsg += `
-┗━━━━━━━━━━━━━━┛\n`
-
-    }
-
-
-
-    menuMsg += `
-
-
-︎┏━━━━━━━━━━━━━━┓
-️┣❏luky md v5 
-┣❏© by frede tech 
-┗━━━━━━━━━━━━━━┛\n
-
-
-┏━━━━━━━━━━━━━━┓
-┃powered by freditech
-┗━━━━━━━━━━━━━━┛\n
-
-
-`;
-
-
+> Hello ${nomAuteurMessage},,, Type menu2 to access a list of commands. 
+  
+╰───────────────────⏣`;
 
    var lien = mybotpic();
 
-
-
    if (lien.match(/\.(mp4|gif)$/i)) {
-
     try {
-
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *LUCKY MD V5*, déveloper Fredie Ezra" , gifPlayback : true }, { quoted: ms });
-
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" , gifPlayback : true }, { quoted: ms });
     }
-
     catch (e) {
-
-        console.log("🥵🥵 Menu error " + e);
-
-        repondre("🥵🥵 Menu error " + e);
-
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
     }
-
 } 
-
 // Vérification pour .jpeg ou .png
-
 else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
-
     try {
-
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *TKM-bot*, déveloper cod3uchiha" }, { quoted: ms });
-
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Luckymdv5*, déveloper Fredie Tech" }, { quoted: ms });
     }
-
     catch (e) {
-
-        console.log("🥵🥵 Menu error " + e);
-
-        repondre("🥵🥵 Menu error " + e);
-
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
     }
-
 } 
-
 else {
-
     
-
     repondre(infoMsg + menuMsg);
-
     
-
 }
 
-
-
-});
-          
+}); 
