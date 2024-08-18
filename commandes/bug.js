5c688425de976dfce762ab7a8ec1edd48b02198a
FredieTech <frediezra60@gmail.com>
2024-08-18 14:55:24 +0300

    Update bug.js

--- commandes/bug.js
+++ commandes/bug.js
@@ -7,12 +7,13 @@ const path = require('path');
 const { generateWAMessageFromContent, proto } = require('@whiskeysockets/baileys');
 
 // bug database
-const { bugtext1 } = require('../framework/bugs/bugtext1');
-const { bugtext2 } = require('../framework/bugs/bugtext2');
-const { bugtext3 } = require('../framework/bugs/bugtext3');
-const { bugtext4 } = require('../framework/bugs/bugtext4');
-const { bugtext5 } = require('../framework/bugs/bugtext5');
-const { bugpdf } = require('../framework/bugs/bugpdf.js')
+const { bugtext1 } = require('../framework/bug/bugtext1');
+const { bugtext2 } = require('../framework/bug/bugtext2');
+const { bugtext3 } = require('../framework/bug/bugtext3');
+const { bugtext4 } = require('../framework/bug/bugtext4');
+const { bugtext5 } = require('../framework/bug/bugtext5');
+const { bugtext5 } = require('../framework/bug/bugtext6');
+const { bugpdf } = require('../framework/bug/bugpdf.js');
 
 
 const category = 'dev';
