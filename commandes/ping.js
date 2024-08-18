7f9dabe00e2473ebbcb902705bc12ce8b4903fb9
FredieTech <frediezra06@gmail.com>
2024-08-18 03:53:50 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*𝐋𝐮𝐜𝐤𝐲 🍀 𝐕𝟓 𝐫𝐞𝐬𝐩𝐨𝐧𝐝 𝐬𝐩𝐞𝐞𝐝 𝐢𝐬*\n ```' +10980+ '``` *m/s*') 
+    return repondre('*𝐋𝐮𝐜𝐤𝐲 🍀 𝐕𝟓 𝐫𝐞𝐬𝐩𝐨𝐧𝐝 𝐬𝐩𝐞𝐞𝐝 𝐢𝐬*${runspeed(process.upspeed())}*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
