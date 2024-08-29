0a2e89700f4cc466b83d32bec42bc232cb8e6ceb
FredieTech <frediezra60@gmail.com>
2024-08-29 18:05:39 +0300

    Create vc_files.js

---
+++ commandes/vc_files.js
@@ -0,0 +1,179 @@
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { getBuffer } = require("../framework/dl/Function");
+const { default: axios } = require('axios');
+
+const runtime = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+ } 
+
+
+zokou({ nomCom: 'vcf',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '📄', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_please wait..._*`) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'getallmembers',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '♻️', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_getting all members_*`) 
+
+   
+
+
+  }
+);
+
+
+
+zokou({ nomCom: 'channel',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🐞', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f`) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'channel1',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🚀', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`top here to join my second channel https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f`) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'update',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '⚙️', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_lucky md is running on its latest vision_*`) 
+
+   
+
+
+  }
+);
+
+
+zokou({ nomCom: 'vision',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🔎', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_lucky md v7_*`) 
+
+   
+
+
+  }
+);
+
+
+  
+zokou({ nomCom: 'luckygc',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '♻️', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`top a link to join lucky md group https://chat.whatsapp.com/HdrwMccSFIaB5Zi7s9BzN9`) 
+
+   
+
+
+  }
+)
+
+
+zokou({ nomCom: 'scriptkidies',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🐅', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`top here to lucky script kiddies group https://chat.whatsapp.com/HdrwMccSFIaB5Zi7s9BzN9`) 
+
+   
+
+
+  }
+)
