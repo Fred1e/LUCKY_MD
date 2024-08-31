7b5d7fb68c4a40d19deb81f0d9aaa76a2f801374
FredieTech <frediezra60@gmail.com>
2024-08-31 23:41:34 +0300

    Update afk.js

--- commandes/afk.js
+++ commandes/afk.js
@@ -4,7 +4,7 @@ const afkfunc = require("../bdd/afk") ;
 
 zokou({
     nomCom : 'afk',
-    categorie : 'Mods',
+    categorie : 'afk',
   } , async (dest,zk,commandeOptions) => {
   
    const {ms , repondre ,superUser , arg} = commandeOptions ;
