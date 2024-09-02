3eb2ad6e2c09fcd675d47d113c08950ab5757b6c
FredieTech <frediezra60@gmail.com>
2024-09-02 23:55:19 +0300

    Update repos.js

--- commandes/repos.js
+++ commandes/repos.js
@@ -4,7 +4,7 @@ const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "repo", catégorie:"Général", reaction: "⚙️", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
   const githubRepo = 'https://api.github.com/repos/Fred1e/LUCKY_MD';
-  const img = 'https://telegra.ph/file/f6c60977ceb194e05e616.jpg';
+  const img = 'https://telegra.ph/file/926c7a8ad7ff624c144b7.jpg';
 
   try {
     const response = await fetch(githubRepo);
