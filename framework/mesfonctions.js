5ff77b2e43a6b824b93680329345da79787aa5e9
FredieTech <frediezra60@gmail.com>
2024-09-03 00:57:24 +0300

    Update mesfonctions.js

--- framework/mesfonctions.js
+++ framework/mesfonctions.js
@@ -9,7 +9,6 @@ const baileys_1 = require("@whiskeysockets/baileys");
 const fs = require('fs-extra');
 const util = require('util');
 let { listall } = require('./stylish-font');
-
 /*_________by Djalega++
 
 fonction zJson:
@@ -33,7 +32,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'Beltah-KE',
+        pack: 'Zokou-MD',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
@@ -274,21 +273,3 @@ function police(text, index) {
     return listall(text)[index];
 }
 exports.police = police;
-
-exports.generatepp = async (buffer) => {
-    const jimp = require('jimp');
-
-    const image = await jimp.read(buffer);
-    const min = image.getWidth();
-    const max = image.getHeight();
-    const cropped = image.clone().crop(0, 0, min, max);
-
-    const imgBuffer = await cropped.clone().scaleToFit(720, 720).getBufferAsync(jimp.MIME_JPEG);
-    const previewBuffer = await cropped.clone().normalize().getBufferAsync(jimp.MIME_JPEG);
-
-    return {
-        img: imgBuffer,
-        preview: previewBuffer
-    };
-}
-
