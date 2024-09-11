b542c6d88e0d5e29e3a7fab44129fc006c96b6f7
FredieTech <frediezra60@gmail.com>
2024-09-12 01:02:46 +0300

    Create react.js

---
+++ commandes/react.js
@@ -0,0 +1,31 @@
+const { zokou } = require('../framework/areact');
+const util = require('areact');
+const axios = require('axios');
+
+zokou({
+    nomCom: "areact",
+    category: "General",
+    reaction: "👌",
+},
+async (Void, citel, text) => {
+if (!isCreator) return m.reply("*📛 THIS IS AN OWNER COMMAND*");
+    let responseMessage;
+
+    if (text === 'on') {
+      config.AUTO_REACT = true;
+      responseMessage = "AUTO_REACT has been enabled.";
+    } else if (text === 'off') {
+      config.AUTO_REACT = false;
+      responseMessage = "AUTO_REACT has been disabled.";
+    } else {
+      responseMessage = "Usage:\n- `autoreact on`: Enable Auto-React\n- `autoreact off`: Disable Auto-React";
+    }
+
+    try {
+      await Matrix.sendMessage(m.from, { text: responseMessage }, { quoted: m });
+    } catch (error) {
+      console.error("Error processing your request:", error);
+      await Matrix.sendMessage(m.from, { text: 'Error processing your request.' }, { quoted: m });
+    }
+  }
+};
