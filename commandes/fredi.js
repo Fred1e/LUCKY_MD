51656508739119781ff1fd5f966b91f3a4794156
FredieTech <frediezra60@gmail.com>
2024-09-13 22:21:18 +0300

    Update fredi.js

--- commandes/fredi.js
+++ commandes/fredi.js
@@ -26,17 +26,17 @@ const temps = moment().format('HH:mm:ss');
 const date = moment().format('DD/MM/YYYY');
   let infoMsg =  `
 ╭━═「 *${s.BOT}* 」═━❂
-┃❖╭───────────➻
-┃❖│◆ 𝙾𝚠𝚗𝚎𝚛 : ${s.OWNER_NAME}
-┃❖│◆ 𝙿𝚛𝚎𝚏𝚒𝚡 : [ ${s.PREFIXE} ] 
-┃❖│◆ 𝙼𝚘𝚍𝚎 : *${mode}*
-┃❖│◆ 𝚁𝚊𝚖  : 𝟴/𝟭𝟯𝟮 𝗚𝗕
-┃❖│◆ 𝙳𝚊𝚝𝚎  : *${date}* 
-┃❖│◆ 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${os.platform()}
-┃❖│◆ 𝙲𝚛𝚎𝚊𝚝𝚘𝚛 : ғʀᴇᴅɪᴇ ᴛᴇᴄʜ
-┃❖│◆ 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜 : ${cm.length}
-┃❖│◆ 𝚃𝚑𝚎𝚖𝚎 : FRED
-┃❖└───────────➻
+┃⊛╭───────────➻
+┃⊛│◆ 𝙾𝚠𝚗𝚎𝚛 : ${s.OWNER_NAME}
+┃⊛│◆ 𝙿𝚛𝚎𝚏𝚒𝚡 : [ ${s.PREFIXE} ] 
+┃⊛│◆ 𝙼𝚘𝚍𝚎 : *${mode}*
+┃⊛│◆ 𝚁𝚊𝚖  : 𝟴/𝟭𝟯𝟮 𝗚𝗕
+┃⊛│◆ 𝙳𝚊𝚝𝚎  : *${date}* 
+┃⊛│◆ 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${os.platform()}
+┃⊛│◆ 𝙲𝚛𝚎𝚊𝚝𝚘𝚛 : ғʀᴇᴅɪᴇ ᴛᴇᴄʜ
+┃⊛│◆ 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜 : ${cm.length}
+┃⊛│◆ 𝚃𝚑𝚎𝚖𝚎 : FRED
+┃⊛└───────────➻
 ╰─━━━━══─⊷─══━━━❂\n${readmore}`;
     let menuMsg = `𝙻𝚞𝚌𝚔𝚢 𝙼𝚍 𝙲𝚖𝚍`;
     for (const cat in coms) {
@@ -46,7 +46,7 @@ const date = moment().format('DD/MM/YYYY');
 └│
 ┌┤ `;for (const cmd of coms[cat]) {
           menuMsg += `          
-│┃┊⊛  *${cmd}*`    
+│┃┊✺  *${cmd}*`    
         } 
         menuMsg +=`
 ┌┤
