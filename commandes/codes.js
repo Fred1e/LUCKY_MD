a8643b6788e5f0a67c6b16b43952dd52543a06ab
FredieTech <frediezra60@gmail.com>
2024-09-13 13:39:44 +0300

    Update codes.js

--- commandes/codes.js
+++ commandes/codes.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
-zokou({ nomCom: "repo", catégorie:"Général", reaction: "⚙️", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "repos", catégorie:"Général", reaction: "⚙️", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
   const githubRepo = 'https://api.github.com/repos/Fred1e/LUCKY_MD';
 
       await handleRepoCommand(m, Matrix, repoUrl);
