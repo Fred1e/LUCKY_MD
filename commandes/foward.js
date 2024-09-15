04d8b3753a1065310aa9127a1b34444e6c1a6cad
FredieTech <frediezra60@gmail.com>
2024-09-16 00:56:01 +0300

    Add files via upload

---
+++ commandes/foward.js
@@ -0,0 +1,23 @@
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { getBuffer } = require("../framework/dl/Function");
+const { default: axios } = require('axios');
+
+zokou(
+ {
+  nomCom: "foward",
+  categorie: "My Contact",
+  desc: "Forwards the replied message",
+  reaction: "🔎",
+  fromMe: "true", 
+ },
+ async (message, match) => {
+  if (!message.quoted) return await message.reply("Reply to message");
+  if (!match) return await message.reply("*Provide a JID; use 'jid' command to get JID*");
+  let jids = parsedJid(match);
+  for (let jid of jids) {
+   await message.client.forwardMessage(jid, message.reply_message.message);
+  }
+  await message.reply("_Message forwarded_");
+ }
+);
