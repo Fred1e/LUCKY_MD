47fbc4d08f5987c117244392f9be0af7da665188
Teeman9 <ngwenyateeman8@gmail.com>
2024-09-27 08:04:33 +0200

    Teeman.set.js

--- set.js
+++ set.js
@@ -1,38 +1,180 @@
-const fs = require('fs-extra');
-const { Sequelize } = require('sequelize');
-if (fs.existsSync('set.env'))
-    require('dotenv').config({ path: __dirname + '/set.env' });
-const path = require("path");
-const databasePath = path.join(__dirname, './database.db');
-const DATABASE_URL = process.env.DATABASE_URL === undefined
-    ? databasePath
-    : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
-    PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "FredieTech",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "Fredie",              
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
-    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
-    BOT : process.env.BOT_NAME || 'LUCKY_𝐌𝐃',
-    URL : process.env.BOT_MENU_LINKS || 'https://telegra.ph/file/926c7a8ad7ff624c144b7.jpg,https://telegra.ph/file/187cfa2365d88ffe98fec.jpg',
-    MODE: process.env.PUBLIC_MODE || "yes",
-    PM_PERMIT: process.env.PM_PERMIT || 'yes',
-    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
-    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
-    WARN_COUNT : process.env.WARN_COUNT || '3' ,
-    ETAT : process.env.PRESENCE || '',
-    CHATBOT : process.env.PM_CHATBOT || 'no',
-    DP : process.env.STARTING_BOT_MESSAGE || "yes",
-    ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
-    DATABASE_URL,
-    DATABASE: DATABASE_URL === databasePath
-        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
-   
-};
-let fichier = require.resolve(__filename);
-fs.watchFile(fichier, () => {
-    fs.unwatchFile(fichier);
-    console.log(`mise à jour ${__filename}`);
-    delete require.cache[fichier];
-    require(fichier);
-});
+const fs = require('fs')
+const chalk = require('chalk')
+
+
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+global.APIKeys = {
+	'https://zenzapis.xyz': 'your key',
+}
+
+const _0x138100=_0x2ef9;(function(_0x11971a,_0x460754){const _0x419220=_0x2ef9,_0x594e91=_0x11971a();while(!![]){try{const _0x46f0c8=-parseInt(_0x419220(0x1b4))/0x1*(parseInt(_0x419220(0x1b1))/0x2)+parseInt(_0x419220(0x1a7))/0x3+parseInt(_0x419220(0x1ae))/0x4*(parseInt(_0x419220(0x1ba))/0x5)+parseInt(_0x419220(0x1a6))/0x6*(parseInt(_0x419220(0x1b9))/0x7)+-parseInt(_0x419220(0x1aa))/0x8*(-parseInt(_0x419220(0x1bb))/0x9)+parseInt(_0x419220(0x1ad))/0xa+-parseInt(_0x419220(0x1a4))/0xb*(parseInt(_0x419220(0x1b7))/0xc);if(_0x46f0c8===_0x460754)break;else _0x594e91['push'](_0x594e91['shift']());}catch(_0x32eb15){_0x594e91['push'](_0x594e91['shift']());}}}(_0x286d,0x1aaae));function _0x286d(){const _0x2089ff=['SERVER','108iDsEXg','sessionidzz','581OZrLVM','142895Vtvtsc','18rHyPnx','BETA\x202.1.0','env','315513omPOfr','SessName','13182FqOGtC','153570ZxdVFx','devNumz','SESSION_ID','168160yXSXYQ','HEROKU_APP_NAME','Local\x20Host','1303410lFUlhr','12nMMtpB','gversion','herokuapp','248564sBtWID','isHeroku','Local','1vLEpWL','bodong'];_0x286d=function(){return _0x2089ff;};return _0x286d();}const {smsg,formatp,tanggal,formatDate,getTime,isUrl,sleep,clockString,runtime,fetchJson,getBuffer,jsonformat,format,parseMention,getRandom}=require('./lib/myfunc');function _0x2ef9(_0x1c4eaf,_0x375c67){const _0x286dd6=_0x286d();return _0x2ef9=function(_0x2ef9c6,_0x16d127){_0x2ef9c6=_0x2ef9c6-0x1a2;let _0x5e257f=_0x286dd6[_0x2ef9c6];return _0x5e257f;},_0x2ef9(_0x1c4eaf,_0x375c67);}global[_0x138100(0x1b2)]=process[_0x138100(0x1a3)][_0x138100(0x1b6)]===undefined?_0x138100(0x1b3):process[_0x138100(0x1a3)][_0x138100(0x1b6)];if(isHeroku===_0x138100(0x1b3))yisHeroku=![];else yisHeroku=!![];if(yisHeroku===![])global[_0x138100(0x1b5)]='./lib/alienalfa.js';global[_0x138100(0x1b8)]=process['env'][_0x138100(0x1a9)]===undefined?![]:process[_0x138100(0x1a3)][_0x138100(0x1a9)],global[_0x138100(0x1af)]=_0x138100(0x1a2),global[_0x138100(0x1b0)]=process[_0x138100(0x1a3)][_0x138100(0x1ab)]===undefined?_0x138100(0x1ac):process[_0x138100(0x1a3)][_0x138100(0x1ab)],global[_0x138100(0x1a5)]='session.alfa.json',global[_0x138100(0x1a8)]=['447405935355','919383400679'];
+
+
+
+
+global.autoTyping = false     //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false  //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = false     //auto reading in gc (true to on, false to off)
+global.autoRecord = true      //auto recording (true to on, false to off)
+global.available = false       //auto available (true to on, false to off)
+global.autoTyping = true      //auto typing (true to on, false to off)
+
+
+
+global.vidmenuz = 'https://tenor.com/bC4cO.gif' //gif and video menu
+
+
+
+
+
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+
+
+
+
+global.vcardowner = ['+263789661013] //ur owner number
+
+global.ownername = 'TEEMAN' //ur owner name
+
+global.ytname = "YT:SOCIAL TODAY" //ur yt chanel name
+
+global.socialm = "Instagram:@teem.an9" //ur github or insta name
+
+global.location = "Zimbabwe,Bulawayo, Cowdray Park " //ur location
+
+
+
+
+global.owner = ['+263789661013']
+
+global.ownertag = '+263789661013' //ur tag number
+
+global.botname = "Social Today services " //ur bot name
+
+global.linkz = '  https://chat.whatsapp.com/BTpNxXRWsaNK8taLkHQwKl ' //your theme url which will be displayed on whatsapp
+
+global.websitex = 'https://www.GITHUB.com/alien-alfa' //ur website to be displayed
+
+global.botscript = 'https://github.com/Alien-Alfa/WhatsAppBot-MD-BETA2' //script link
+
+global.reactmoji =  "™️" //ur menu react emoji
+
+global.themeemoji = "™️" //ur theme emoji
+
+global.packname = ' Teeman Stickers' //ur sticker watermark packname
+
+global.autorname = 'Teeman ' //ur sticker watermark author
+
+
+
+
+
+global.thumz = 'https://github.com/Alien-alfa/Alien-alfa/blob/beta/Alfa.jpg?raw=true' //ur thumb pic
+
+global.logoz = 'https://github.com/Alien-alfa/Alien-alfa/blob/beta/Alfa.jpg?raw=true' //ur logo pic
+
+global.err4rz = 'https://github.com/Alien-alfa/Alien-alfa/blob/beta/Alfa.jpg?raw=true' //ur error pic
+
+global.thumbz = 'https://github.com/Alien-alfa/Alien-alfa/blob/beta/Alfa.jpg?raw=true' //ur thumb pic
+
+
+
+
+
+global.premium = global.premiumzz || ['263789661013'] //ur premium numbers
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+
+
+
+global.antitags = global. true
+global.prefa = ['','!','.','#','🐤','🗿']
+global.sp = '𒆜'
+global.mess = {
+    success: 'Done ✓',
+    admin: 'This Feature Is Only For President !',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This Feature Is Only For President !',
+    group: 'Feature Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This Feature Is Only For Bot',
+    wait: '```Processing...```',
+    linkm: 'Where is the link?',
+    error: 'Error!!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    ban: 'You have been banned by the Presidential Admin , if you want to be unbanned, chat Admins.',
+    nsfw: 'The nsfw feature has not been activated, please contact the President to activate',
+    banChat: 'The bot was banned in this group, please contact the President unban'
+}
+
+global.SessName = ' eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTUpRRTRMTUhEY0tqQkJvNEhaSkNjSXFDdWlXdzdEV0lxdG0xMmN3VGlITT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoia2lZMCtSbVNOTE9pWnNxZlU1YlY4T2VPcldyekhsMWlieU9qN1FJL0dWcz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJBTXNSSlAzb09udmF2MUJHVXJXWDVvZkNVTFFIdC9sdm1OKzQwRlpscjFNPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ6V3VwZFRUK0lJdlIvSTBGelFhdEQwZDFPaWsxZWhWbWZxNFROYXluUVI4PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjhBNXNJSEV6clRtck1LTlpFNThLVlpPNE5VT1ZPVUlWUHphL3RPUWwwVzg9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlA4dlM0OFE3TlVUUThNSlo4eEd2OEtJaDlEbS91ODVoUDBuOGpjdjdCSHc9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidUFvWStQWURmUXJvTEtnVXlWNVVwbnUzMHhMNE5LcjlHUFR2S2ZmdzRIdz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUml3ZXc2TkhWQndCS2tGNFVTektDYWVKQ2hTSTVpem01OUFGcU95by8yND0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjRBZWEzT202bGc1TGErZGFWN2RZR0NNMTBTbmJnRS8vOG9tS2hrRVpldVhBZTZVNk1tV2M2eG5sQmZPMGNxRkxEcDhSa1J3NkIwS2p0eEhTck1JcENRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTM5LCJhZHZTZWNyZXRLZXkiOiJDVmh2UWVzZE9jbThBNEh2cm5adkxXSzd5WGdNQjVnRE1zZnNjcVQ4UjFnPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJxNFlFMkR1M1RXYUFnLU5DaTZEcXVnIiwicGhvbmVJZCI6IjBiNmZiN2E2LTA3ODMtNDY1ZC1hOGJhLTI0OWY4NzUyZDhmZSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJQV3ZZL1NCbUR0UUkrY3p3ZlQvRTFGQ1pVTGM9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK3BXcDVUT2hRZ0RjR3UrTWhhV3FQK1JrTDdFPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IkRYTjZEWFZHIiwibWUiOnsiaWQiOiIyNjM3ODk2NjEwMTM6MUBzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiJUZWVtYW4ifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ09tdW14VVFwdGZZdHdZWUFTQUFLQUE9IiwiYWNjb3VudFNpZ25hdHVyZUtleSI6InF2cEtPMXZNbWdVb0d4Zk5RNk5Pem94cklPalYwT0VPU0FnWmpTbmxwRms9IiwiYWNjb3VudFNpZ25hdHVyZSI6InpqQkkyMXc3UDBwY1pMTEExZHlTWmRvQUM2QmF4dFpyZWJ6RnF2YytlMmhlT1FnT05CL0cvSG15RTlPN1FRWGZDUURWQlY0YXZFWWh2OHNZS2VKeENBPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJtZ1RxeUNia3lZUUw1L2NVYzdOZ0NQM1B4MFJGRUk5WUdiVThNUXZuTUEralpDTzRmT2xxRUJTeVhtSWI2NlNNaDYrNlZSazBpZnZobEJZWWs5OTdBZz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjI2Mzc4OTY2MTAxMzoxQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmFyNlNqdGJ6Sm9GS0JzWHpVT2pUczZNYXlEbzFkRGhEa2dJR1kwcDVhUloifX1dLCJwbGF0Zm9ybSI6InNtYmEiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3Mjc0MDkwNzZ9 '
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+let worktypez = process.env.WORKTYPE === undefined ? 'Private' : process.env.WORKTYPE
+//-------------
+let workres = worktypez.toLowerCase()  
+//-------------
+if (workres === 'public') {global.worktype = 'public'} 
+//-------------
+else {global.worktype = 'public'}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+global.gversion = '1.8.0 BETA TEST'
+global.herokuapp = 'Local Host'
+
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
