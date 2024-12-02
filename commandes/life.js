1f11598d015b04a4406162a95f84e5926328beed
devfreetec <devfreetech@yahoo.com>
2024-12-02 08:14:11 +0300

    Add files via upload

---
+++ commandes/life.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+zokou({ nomCom: "test", reaction: "💐", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = '𝐡𝐞𝐥𝐥𝐨𝐰 𝐭𝐡𝐢𝐬 𝐢𝐬 *LUCKY MD  * \n\n ' + "i'm a whatsapp bot multi-device created ";
+    let d = ' by *frediezra255 *';
+    let varmess = z + d;
+    var img = 'https://files.catbox.moe/7irwqn.jpeg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+/*module.exports.commande = () => {
+  var nomCom = ["test","t"]
+  var reaction="😁"
+  return { nomCom, execute,reaction }
+};
+
+async function  execute  (origineMessage,zok) {
+  console.log("Commande saisie !!!s")
+   let z ='Hi my name is *LUCKY MD* \n\n '+' I'm a Multi-device Whatsapp bot '
+      let d =' developed by *frediezra255*'
+      let varmess=z+d
+      var img='https://files.catbox.moe/7irwqn.jpeg'
+await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
+}  */ 
