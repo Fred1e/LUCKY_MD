7b31393ce30f8c927503123fba5479bd946f7bcd
devfreetec <devfreetech@yahoo.com>
2024-12-14 07:43:56 +0300

    Update and rename abot-owners-gm.js to apair.js

---
+++ commandes/apair.js
@@ -0,0 +1,37 @@
+const {
+  zokou
+} = require("../framework/zokou");
+const {
+  default: axios
+} = require("axios");
+zokou({
+  'nomCom': "pair",
+  'aliases': ["session", "pair", "paircode", "qrcode"],
+  'reaction': '♂️',
+  'categorie': "General"
+}, async (_0x2ce843, _0x1c44fd, _0x32de8a) => {
+  const {
+    repondre: _0x2e61d5,
+    arg: _0x8621a4
+  } = _0x32de8a;
+  try {
+    if (!_0x8621a4 || _0x8621a4.length === 0) {
+      return _0x2e61d5("Example Usage: .code 25575259xxxx.");
+    }
+    await _0x2e61d5("*Wait lucky-md is generating your pair code ✅...*");
+    const _0x386b0a = encodeURIComponent(_0x8621a4.join(" "));
+    const _0x1ea92d = "https://fredietech.onrender.com/code?number=" + _0x386b0a;
+    const _0xb59e41 = await axios.get(_0x1ea92d);
+    const _0x1b71f0 = _0xb59e41.data;
+    if (_0x1b71f0 && _0x1b71f0.code) {
+      const _0x40751a = _0x1b71f0.code;
+      await _0x2e61d5('' + _0x40751a);
+      await _0x2e61d5("Here is your pair code, copy and paste it to the notification above or link devices.");
+    } else {
+      throw new Error("Invalid response from API.");
+    }
+  } catch (_0x21fdc6) {
+    console.error("Error getting API response:", _0x21fdc6.message);
+    _0x2e61d5("Error getting response from API.");
+  }
+});
