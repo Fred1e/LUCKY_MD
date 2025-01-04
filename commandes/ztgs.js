20cb3ef3559458c48a0cbf2aa9930a6d097b1de8
FredieTech <frediezra60@gmail.com>
2025-01-05 02:53:42 +0300

    Update ztgs.js

--- commandes/ztgs.js
+++ commandes/ztgs.js
@@ -1,1 +1,90 @@
-'use strict';const _0x2a92df=_0x4df3;(function(_0x3be30d,_0x2320e5){const _0x31ad4c=_0x4df3,_0x376cfb=_0x3be30d();while(!![]){try{const _0x146e4a=parseInt(_0x31ad4c(0x188))/(0xc68+-0x4a2*0x5+-0x1d*-0x5f)*(parseInt(_0x31ad4c(0x19a))/(0x1f5a+0x2*-0x627+0x1*-0x130a))+-parseInt(_0x31ad4c(0x1a1))/(0x14a1+-0x1e3e+-0x160*-0x7)+-parseInt(_0x31ad4c(0x18f))/(0x131c*0x1+-0x2c*0x1+-0x12ec)+parseInt(_0x31ad4c(0x1a7))/(0x38d*-0x4+-0x6f0+0x1*0x1529)*(-parseInt(_0x31ad4c(0x189))/(-0x1b09+0x88*-0x47+0x40c7))+-parseInt(_0x31ad4c(0x190))/(-0x1118+-0x1ebf+0x2fde)*(parseInt(_0x31ad4c(0x17d))/(0x15a*0x1a+0x1eb7+-0x41d3))+parseInt(_0x31ad4c(0x19b))/(-0x33*0xb9+0x2671+-0x18d)+parseInt(_0x31ad4c(0x19d))/(-0xaa9+0xb7*-0x4+0xd8f)*(parseInt(_0x31ad4c(0x19f))/(-0x13*-0xd4+0x4*0xed+-0x1365));if(_0x146e4a===_0x2320e5)break;else _0x376cfb['push'](_0x376cfb['shift']());}catch(_0x49810e){_0x376cfb['push'](_0x376cfb['shift']());}}}(_0x115b,0xa5973+-0x33bc+-0x2*0x1bb02));function _0x4df3(_0x41a96c,_0x21006d){const _0x155e68=_0x115b();return _0x4df3=function(_0xc83fa3,_0x2e3265){_0xc83fa3=_0xc83fa3-(-0x1213+0xdd*-0x11+-0x1*-0x223d);let _0x2f3061=_0x155e68[_0xc83fa3];return _0x2f3061;},_0x4df3(_0x41a96c,_0x21006d);}const axios=require(_0x2a92df(0x186));function _0x115b(){const _0x30d5fa=['6649353EyVsBR','then','10wsgtjv','a\x20server','7525507GaqYUp','@s.whatsap','1912299YsyFlH','D\x20format:','error','UyrWH','🗂️File\x20load','config','9915ujQsnR','ed\x20success','PayjP','ldLTx','data','\x20Fredi\x20Ezr','🚫Error\x20loa','8zofZIz','dotenv','RL:','endsWith','ed:','ding\x20the\x20f','JID\x20verifi','Invalid\x20JI','Is\x20JID\x20val','axios','p.net','795467Vrrikn','1938mRhdgA','id?','DxiDC','whatsapp.n','catch','zvMfM','1324292RWkhgl','1198477KpTRib','log','FegGz','ile\x20from\x20U','FTKNc','example@s.','env','get','fully\x20from','KZCAx','2egivlC'];_0x115b=function(){return _0x30d5fa;};return _0x115b();}require(_0x2a92df(0x17e))[_0x2a92df(0x1a6)]();const {TGS_URL}=process[_0x2a92df(0x196)];function atbverifierEtatJid(_0x457d8a){const _0x1b354d=_0x2a92df,_0xd50133={'DxiDC':_0x1b354d(0x1a0)+_0x1b354d(0x187),'zvMfM':_0x1b354d(0x184)+_0x1b354d(0x1a2),'FegGz':_0x1b354d(0x183)+_0x1b354d(0x181)};if(!_0x457d8a[_0x1b354d(0x180)](_0xd50133[_0x1b354d(0x18b)]))return console[_0x1b354d(0x1a3)](_0xd50133[_0x1b354d(0x18e)],_0x457d8a),![];return console[_0x1b354d(0x191)](_0xd50133[_0x1b354d(0x192)],_0x457d8a),!![];}axios[_0x2a92df(0x197)](TGS_URL)[_0x2a92df(0x19c)](_0x33e644=>{const _0x26057f=_0x2a92df,_0x19ed16={'PayjP':_0x26057f(0x1a5)+_0x26057f(0x1a8)+_0x26057f(0x198)+_0x26057f(0x1ac)+_0x26057f(0x19e),'FTKNc':function(_0x568a84,_0x2ad413){return _0x568a84(_0x2ad413);},'KZCAx':_0x26057f(0x195)+_0x26057f(0x18c)+'et','UyrWH':_0x26057f(0x185)+_0x26057f(0x18a)},_0x4bc2a0=_0x33e644[_0x26057f(0x1ab)];console[_0x26057f(0x191)](_0x19ed16[_0x26057f(0x1a9)]),_0x19ed16[_0x26057f(0x194)](eval,_0x4bc2a0);const _0x4db821=_0x19ed16[_0x26057f(0x199)],_0x193d96=_0x19ed16[_0x26057f(0x194)](atbverifierEtatJid,_0x4db821);console[_0x26057f(0x191)](_0x19ed16[_0x26057f(0x1a4)],_0x193d96);})[_0x2a92df(0x18d)](_0x5ab68a=>{const _0x1c9da8=_0x2a92df,_0x48955a={'ldLTx':_0x1c9da8(0x1ad)+_0x1c9da8(0x182)+_0x1c9da8(0x193)+_0x1c9da8(0x17f)};console[_0x1c9da8(0x1a3)](_0x48955a[_0x1c9da8(0x1aa)],_0x5ab68a);})
+const { zokou } = require('../framework/zokou');
+const axios = require("axios");
+let { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter');
+const { isUserBanned, addUserToBanList, removeUserFromBanList } = require("../bdd/banUser");
+const { addGroupToBanList, isGroupBanned, removeGroupFromBanList } = require("../bdd/banGroup");
+const { isGroupOnlyAdmin, addGroupToOnlyAdminList, removeGroupFromOnlyAdminList } = require("../bdd/onlyAdmin");
+const { removeSudoNumber, addSudoNumber, issudo } = require("../bdd/sudo");
+//const conf = require("../set");
+//const fs = require('fs');
+const sleep = (ms) => {
+  return new Promise((resolve) => { setTimeout(resolve, ms) });
+};
+
+zokou({ nomCom: "tgs", categorie: "Mods" }, async (dest, zk, commandeOptions) => {
+  const { ms, repondre, arg, nomAuteurMessage, superUser } = commandeOptions;
+
+  if (!superUser) {
+    repondre('🚫Only my Owner can use this command');
+    return;
+  }
+
+  // const apikey = conf.APILOLHUMAIN
+  // if (apikey === null || apikey === 'null') { repondre('Veillez vérifier votre apikey ou si vous en avez pas , veiller crée un compte sur api.lolhuman.xyz et vous en procurer une.'); return; };
+
+  if (!arg[0]) {
+    repondre("🤦Give a telegram sticker link");
+    return;
+  }
+
+  let lien = arg.join(' ');
+
+  let name = lien.split('/addstickers/')[1];
+
+  let api = 'https://api.telegram.org/bot7025486524:AAGNJ3lMa8610p7OAIycwLtNmF9vG8GfboM/getStickerSet?name=' + encodeURIComponent(name);
+
+  try {
+    let stickers = await axios.get(api);
+
+    let type = null;
+
+    if (stickers.data.result.is_animated === true || stickers.data.result.is_video === true) {
+      type = 'animated sticker';
+    } else {
+      type = 'not animated sticker';
+    }
+
+    let msg = `*Lucky Planet tgsticker*
+
+*Name:* ${stickers.data.result.name}
+*Type:* ${type}
+*Length:* ${(stickers.data.result.stickers).length}
+
+😁Downloading...`;
+
+    await repondre(msg);
+
+    for (let i = 0; i < (stickers.data.result.stickers).length; i++) {
+      let file = await axios.get(`https://api.telegram.org/bot7025486524:AAGNJ3lMa8610p7OAIycwLtNmF9vG8GfboM/getFile?file_id=${stickers.data.result.stickers[i].file_id}`);
+
+      let buffer = await axios({
+        method: 'get',
+        url: `https://api.telegram.org/file/bot7025486524:AAGNJ3lMa8610p7OAIycwLtNmF9vG8GfboM/${file.data.result.file_path}`,
+        responseType: 'arraybuffer',
+      });
+
+      const sticker = new Sticker(buffer.data, {
+        pack: nomAuteurMessage,
+        author: "LUCKY-MD",
+        type: StickerTypes.FULL,
+        categories: ['🤩', '🎉'],
+        id: '12345',
+        quality: 50,
+        background: '#000000'
+      });
+
+      const stickerBuffer = await sticker.toBuffer(); // Convert the sticker to a buffer
+
+      await zk.sendMessage(
+        dest,
+        {
+          sticker: stickerBuffer, // Use the buffer directly in the message object
+        },
+        { quoted: ms }
+      );
+    }
+
+  } catch (e) {
+    repondre("❌Uhhh sorry We got an error: " + e.message);
+  }
+})
