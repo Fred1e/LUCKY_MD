af7f77b862386b96db4b4b2c423b03d1a876e33d
FredieTech <frediezra60@gmail.com>
2025-02-08 16:52:50 +0300

    Update menu1.js

--- luckycmd/menu1.js
+++ luckycmd/menu1.js
@@ -8,7 +8,7 @@ const s = require(__dirname + "/../set");
 const more = String.fromCharCode(8206);
 const readmore = more.repeat(4001);
 
-ezra({ nomCom: "menu", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
+ezra({ nomCom: "menu1", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
     let { ms, repondre, prefixe, nomAuteurMessage, mybotpic } = commandeOptions;
     let { cm } = require(__dirname + "/../fredi/ezra");
     let coms = {};
